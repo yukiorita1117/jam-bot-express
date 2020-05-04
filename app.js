49bd8887ee2d22cbd4246f10017404b1de252a22
折田幸夫 <3204704@gmail.com>
2020-05-04 20:14:08 +0900

    slack bot 機能実装

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const { WebClient } = require("@slack/web-api");
 
 var indexRouter = require("./routes/index");
 
@@ -30,8 +31,22 @@ app.use(bodyParser.json());
 
 // slack bot
 app.post("/", function (req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.send(req.body.challenge);
+  // Slackは"Request URL"に設定したURLが正当なものかを判定するため、
+  // Request URLに指定した内容にPOSTリクエストを送る。
+  // それに対し、受け取ったデータから"Challenge"を抜き出して送信する必要がある。
+  // res.setHeader("Content-Type", "text/plain");
+  // res.send(req.body.challenge);
+
+  console.log(req.body.event);
+  // botからの発言でなく、100文字以上のmessageイベントにリプライ
+  if (!req.body.event.bot_id && [...req.body.event.text].length >= 100) {
+    // chat.postMessageの実行
+    web.chat.postMessage({
+      as_user: true,
+      channel: req.body.event.channel,
+      text: `<@${req.body.event.user}> めっちゃ早口で言ってそう`,
+    });
+  }
 });
 
 app.listen(8080);
