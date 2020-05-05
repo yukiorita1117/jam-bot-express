2ca9ba2aca1975713226a557927fa6191e1184d1
折田幸夫 <3204704@gmail.com>
2020-05-05 18:30:40 +0900

    index routeの 削除

--- app.js
+++ app.js
@@ -66,9 +66,6 @@ app.post("/", function (req, res) {
 
 // -------------------------------------------------------------------  //
 
-// 今回は root を使いたいのでコメントアウト
-// app.use('/', indexRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
